29feff366540567b2c0a7e156bbcd9494ea69f3a
Pedro Lara <pedroalveslara@gmail.com>
2022-06-23 17:30:48 -0300

    Jogos concluídos

--- Basics/jogoDaForca.py
+++ Basics/jogoDaForca.py
@@ -1,27 +1,44 @@
+import random
+from signal import pause
+
 def jogar_forca():
 
     print('***************************')
     print('Bem vindo ao jogo da Forca!')
     print('***************************')
 
-    palavra_secreta = 'banana'
+    file = open('forca.txt', 'r')
 
-    letras_acertadas = ['_']*len(palavra_secreta)
+    palavras = []
+
+    for linha in file:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    numero = random.randrange(0, len(palavras))
+
+    palavra_secreta = palavras[numero].lower()
+
+    letras_acertadas = ['_' for letra in palavra_secreta]
+
+    tentativas_de_letras = letras_acertadas.__len__()
 
     enforcado = False
 
     acertou = False
 
-    tentativas_de_letras = letras_acertadas.__len__()
-
-    arquivo = open('palavras.txt', 'r')
-
-    arquivo.read()
+    chances = palavra_secreta.__len__()
 
     while(not enforcado and not acertou):
+
+        print('Você tem {} tentativas!'.format(chances))
+        
         print(letras_acertadas)
+        
         chute = input('Qual letra você deseja chutar: ')
+        
         print('***************************')
+        
         chute = chute.lower()
 
         if(chute in palavra_secreta):
@@ -39,11 +56,12 @@ def jogar_forca():
                     break
         else:
             tentativas_de_letras -= 1
+            chances -= 1
             if(tentativas_de_letras == 0):
                 enforcado = True
                 print('***************************')
                 print('Você perdeu!')
-                print('A palavra secreta era: {}'.format(palavra_secreta))
+                print('A palavra secreta era: {}'.format(palavra_secreta).upper())
 
 if(__name__ == '__main__'):
     jogar_forca()
