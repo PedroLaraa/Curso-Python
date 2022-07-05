f9177b58682f8a9f424c2eb30aa1c165d96f5d7d
Pedro Lara <pedroalveslara@gmail.com>
2022-07-05 13:17:06 -0300

    Update main.py

--- CursoPythonPoo/Strings em Py - Extrator URL/main.py
+++ CursoPythonPoo/Strings em Py - Extrator URL/main.py
@@ -1,3 +1,11 @@
-url = 'http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100'
+url = 'https://bytebank.com/cambio?moedaOrigem'
 
-print(url)
+indice_interrogacao = url.find('?')
+
+url_base = url[0 :indice_interrogacao]
+
+print(url_base)
+
+url_params = url[indice_interrogacao + 1: len(url) + 1]
+
+print(url_params)
