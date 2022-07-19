5c6a0f653e63060600d08f703fac7fe687eb2275
Pedro Lara <pedroalveslara@gmail.com>
2022-07-19 11:47:53 -0300

    Curso: Listas e Tuplas 1 finalizado =D

--- CursoPythonPoo/Listas&Tuplas/ListasComObj.py
+++ CursoPythonPoo/Listas&Tuplas/ListasComObj.py
@@ -15,6 +15,11 @@ class Conta:
     def __str__(self):
         return '[>>Codigo {} | Saldo {}<<]'.format(self._codigo, self._saldo)
 
+    def __lt__(self, outro):
+        if self._saldo != outro._saldo:
+            return self._saldo < outro._saldo
+        return self._codigo < outro._codigo
+
 class ContaCorrente(Conta):
     
     def passa_o_mes(self):
@@ -37,10 +42,13 @@ conta_da_maria.deposita(444)
 
 # print(conta_da_maria)
 
-contas = [conta_da_maria, conta_do_pedro]
+contas = [conta_do_pedro, conta_da_maria]
 
-for conta in contas:
+# def extrai_saldo(conta):
+#     return conta._saldo
+
+for conta in sorted(contas, reverse=True):
     conta.passa_o_mes()
     print(conta)
 
-
+print(conta_do_pedro < conta_da_maria) # FALSE
